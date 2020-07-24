1ec6af9be91e31478d00918f9d66c74386a6e08d
Le Minh Tri <ansidev@gmail.com>
2020-07-24 17:41:34 +0700

    Update Role Permission API

--- backend/app/Http/Controllers/RoleManager.php
+++ backend/app/Http/Controllers/RoleManager.php
@@ -2,26 +2,29 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Spatie\Permission\Models\Role;
-use Spatie\Permission\Models\Permission;
 use App\User;
+use Illuminate\Http\Request;
+use Spatie\Permission\Exceptions\RoleDoesNotExist;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class RoleManager extends Controller
 {
     public function permissionsIndex()
     {
-        return Permission::all()->paginate(100);
+        return Permission::all();
     }
-
 
     public function rolesIndex()
     {
-        return Role::all()->paginate(100);
+        return Role::all();
     }
 
-    public function rolesAddUser(Request $request, Role $role, User $user)
+    public function assignRoleToUser(Request $request)
     {
+        /** @var Role $role */
+        /** @var User $user */
+        list($role, $user) = $this->getRoleAndUser($request);
 
         $user->assignRole($role);
 
@@ -30,13 +33,41 @@ class RoleManager extends Controller
         ], 200);
     }
 
-
-    public function rolesRemoveUser(Request $request, Role $role, User $user)
+    public function unassignRoleToUser(Request $request, Role $role, User $user)
     {
+        /** @var Role $role */
+        /** @var User $user */
+        list($role, $user) = $this->getRoleAndUser($request);
+
         $user->removeRole($role);
 
         return response()->json([
             "message" => $role->name . " Role successfully removed from User"
         ], 200);
+    }
+
+    private function getRoleAndUser(Request $request) {
+        $role_id = $request->json()->get("role_id");
+        $user_id = $request->json()->get("user_id");
+
+        try {
+            /** @var Role $role */
+            $role = Role::findById($role_id);
+        } catch (RoleDoesNotExist $roleDoesNotExist) {
+            return response()->json([
+                "message" => "Role not found"
+            ], 404);
+        }
+
+        /** @var User $user */
+        $user = User::where('id', $user_id)->first();
+
+        if (is_null($user)) {
+            return response()->json([
+                "message" => "User not found"
+            ], 404);
+        }
+
+        return [$role, $user];
     }
 }
